5bc964daf64fb81711753d83d4360ad1c6978e64
Hristo Enev <hristo@Hristos-MacBook-Pro.local>
2016-10-04 10:37:18 +0300

    Created node server file for heroku deployment

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ var path = require('path')
 var app = express();
 
 app.use(express.static(path.join(__dirname, '/app')));
+
+app.get('/', function(req, res) {
+    res.sendfile('/index.html');
+});
+
 app.listen(process.env.PORT || 3000);
