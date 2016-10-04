10eb7cb53e7aefce393ee9c81e42d9f89b20ccc9
Hristo Enev <hristo@Hristos-MacBook-Pro.local>
2016-10-04 10:46:12 +0300

    Created node server file for heroku deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, '/app')));
 
 app.get('/', function(req, res) {
-    res.sendfile('/index.html');
+    res.sendfile('./index.html');
 });
 
 app.listen(process.env.PORT || 3000);
