222c676f8c95112321355201b89ab29011568ddc
Hristo Enev <hristo@Hristos-MacBook-Pro.local>
2016-10-04 11:06:52 +0300

    Created node server file for heroku deployment

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var path = require('path')
 var app = express();
 
 app.use(express.static(path.join(__dirname, '/')));
-// app.use(express.static(path.join(__dirname, '/node_modules')));
-// app.use(express.static(path.join(__dirname, '/assets')));
 
 app.get('/', function(req, res) {
     res.sendfile('./index.html');
